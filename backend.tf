b16d90c282f3111e54e9f141db9539ac5c91bf2b
rohithlh <84400715+rohithlh@users.noreply.github.com>
2026-03-27 23:16:53 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-tf-state-bucket"
-    key            = "project/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
